31d7e9843aa5393cc3c76b30851a9dcf5a5cbe4b
Jim Birch <5177009+thejimbirch@users.noreply.github.com>
2024-03-07 12:22:07 -0500

    Adds Critical CSS Scripts and dependencies (#23)

---
+++ critical-css/clean.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function deleteFolderRecursive(path) {
+  if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
+    fs.readdirSync(path).forEach(function (file) {
+      var curPath = path + '/' + file;
+
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // recurse
+        deleteFolderRecursive(curPath);
+      } else {
+        // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+
+    console.log(`Deleting directory "${path}"...`);
+    fs.rmdirSync(path);
+  }
+}
+
+console.log('Cleaning working tree...');
+
+deleteFolderRecursive(
+  `${process.env.PROJECT_ROOT}/${process.env.DOCROOT}/${process.env.THEME}/critical-css/css`,
+);
+
+console.log('Successfully cleaned working tree!');
