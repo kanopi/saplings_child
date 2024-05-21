91357f463c621008595100974f2ae20b3662eebe
Nicole Karhoff <45244877+nkarhoff@users.noreply.github.com>
2024-05-21 10:39:47 -0400

    TW29486487 Animated Stats (#39)
    * Add animated stat pattern
    
    * Update to move item to Columns component

---
+++ templates/patterns/animated-stat/js/animated-statistics.js
@@ -0,0 +1,51 @@
+/**
+ * Animated Stats JS.
+ * @file
+ */
+(function (Drupal, drupalSettings) {
+    /*
+    * Animate Stats.
+    */
+    Drupal.behaviors.animateStat = {
+        attach: function (context) {
+ 
+          const elements = document.querySelectorAll('.stat_paragraph');
+ 
+          elements.forEach(function(element) {
+            const observer = new IntersectionObserver(entries => {
+              if (entries[0].isIntersecting) {
+                setTimeout(function() {
+                  const counters = element.querySelectorAll('.stat_number');
+                  const speed = 500;
+                  
+                  counters.forEach( counter => {
+                    const animate = () => {
+                        const value = +counter.getAttribute('value');
+                        const data = +counter.innerText;
+                      
+                        const time = value / speed;
+                        if (data < value) {
+                          if (value > 999) {
+                            counter.innerText = Math.ceil(data + time + 100);
+                            setTimeout(animate, 1);
+                          } else {
+                            counter.innerText = Math.ceil(data + time);
+                            setTimeout(animate, 1);
+                          }
+                        } else {
+                          counter.innerText = value;
+                        }
+                    }
+                    animate();
+                  });
+                }, 1000);
+              }
+            });
+            observer.observe( element );
+          });
+ 
+          
+ 
+        },
+      };
+  })(Drupal, drupalSettings);
